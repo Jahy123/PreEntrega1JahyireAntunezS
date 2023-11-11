78c3642cab77444072edd9eb29c95d4f5fbdcefc
Jahyire Antunez <jahyireantunezsalazar@gmail.com>
2023-11-11 16:48:04 -0600

    Preparacion tres  para entrega final

--- src/components/pages/notFound/NotFound.jsx
+++ src/components/pages/notFound/NotFound.jsx
@@ -1,5 +1,17 @@
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../../../firebaseConfig";
+import styles from "./NotFound.module.css";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <section className={styles.seccion}>
+      <img
+        className={styles.img}
+        src="https://res.cloudinary.com/drqhvncqw/image/upload/v1699741793/Dise%C3%B1o_sin_t%C3%ADtulo_3_zplxgx.png"
+        alt="Página de no encontrado/a"
+      />
+    </section>
+  );
 };
 
 export default NotFound;
